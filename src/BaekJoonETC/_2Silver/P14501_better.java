d630c963c764c71f5887c2bb2171b8cfe0f684e7
Junmin <junmink1223@gmail.com>
2023-02-12 12:21:31 +0900

    BOJ

--- src/BaekJoonETC/_2Silver/P14501_better.java
+++ src/BaekJoonETC/_2Silver/P14501_better.java
@@ -6,24 +6,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class P14501 {
+public class P14501_better {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[][] dp = new int[n+1][n+1];
+        int[] dp = new int[n+1];
         StringTokenizer st;
         int day, pay;
-        for (int i = 1; i < dp.length; i++) {
+        for (int i = 1; i < n+1; i++) {
             st = new StringTokenizer(br.readLine());
             day = Integer.parseInt(st.nextToken());
             pay = Integer.parseInt(st.nextToken());
-            for (int j = i; j < dp[0].length; j++) {
-                if(day+i-1>j) dp[i][j] = dp[i-1][j];
-                else {
-                    dp[i][j] = Math.max(dp[i-1][j], pay+dp[i-1][i-1]);
-                }
+            if(i+day-1<n+1) {
+                dp[i+day-1] = Math.max(dp[i+day-1], pay + dp[i-1]);
             }
+
+            dp[i] = Math.max(dp[i], dp[i-1]);
         }
-        System.out.println(dp[n][n]);
+        System.out.println(dp[n]);
     }
 }
