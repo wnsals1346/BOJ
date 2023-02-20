805f1664bf1b659ab744b906bff3a7891d25ae05
Junmin <junmink1223@gmail.com>
2023-02-20 10:21:34 +0900

    BOJ

--- src/BaekJoonETC/_2Silver/P11725.java
+++ src/BaekJoonETC/_2Silver/P11725.java
@@ -4,41 +4,43 @@ package BaekJoonETC._2Silver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class P11725 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int vertex = Integer.parseInt(br.readLine());
-        int[][] matrix = new int[vertex][vertex];
-        int[] res = new int[vertex];
-        boolean[] visited = new boolean[vertex];
+        int v = Integer.parseInt(br.readLine());
+        List<Integer>[] list = new ArrayList[v+1];
+        int[] res = new int[v+1];
         StringTokenizer st;
-
-        while(vertex-->1) {
-            st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken())-1;
-            int to = Integer.parseInt(st.nextToken())-1;
-            matrix[from][to] = matrix[to][from] = 1;
+        for (int i = 1; i < list.length; i++) {
+            list[i] = new ArrayList<>();
         }
 
-        ArrayList<Integer> queue = new ArrayList<>();
-        queue.add(0);
-        visited[0] = true;
+        while(v-->1) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            list[from].add(to);
+            list[to].add(from);
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        res[1] = 1;
 
         while(queue.size()!=0) {
-            int cur = queue.remove(0);
-            for (int i = 0; i < matrix.length; i++) {
-                if(matrix[cur][i]==1 && !visited[i]) {
-                    queue.add(i);
-                    res[i] = cur+1;  // 현재 부모를 배열에 저장
-                    visited[i] = true;
+            int cur = queue.poll();
+            for (int i = 0; i < list[cur].size(); i++) {
+                int next = list[cur].get(i);
+                if(res[next]==0) {
+                    queue.add(next);
+                    res[next] = cur;  // 현재 부모를 배열에 저장
                 }
             }
         }
         StringBuilder sb = new StringBuilder();
-        for (int i = 1; i < res.length; i++) {
+        for (int i = 2; i < res.length; i++) {
             sb.append(res[i]).append("\n");
         }
         System.out.println(sb);
