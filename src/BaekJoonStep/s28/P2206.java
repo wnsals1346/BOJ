84f65900c495c0afa20356df462f0f9cd3be8c97
Junmin <junmink1223@gmail.com>
2023-02-23 12:58:44 +0900

    BOJ

--- src/BaekJoonStep/s28/P2206.java
+++ src/BaekJoonStep/s28/P2206.java
@@ -4,62 +4,75 @@ package BaekJoonStep.s28;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class P2206 {
-    static int[][] map;
-    static int X, Y, min = Integer.MAX_VALUE;
+    static int[][][] map;
+    static int X, Y;
     static int[] dx = {-1,1,0,0}, dy = {0,0,-1,1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br. readLine());
         X = Integer.parseInt(st.nextToken());
         Y = Integer.parseInt(st.nextToken());
-        map = new int[X][Y];
+        map = new int[X][Y][2];  // 벽을 안부순 경우 0, 벽을 부순 경우 1 따로 체크한다.
         for (int i = 0; i < X; i++) {
             int[] line = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
             for (int j = 0; j < Y; j++) {
-                map[i][j] = line[j];
+                map[i][j][0] = line[j];
+                map[i][j][1] = line[j];
             }
         }
-        dfs(0, 0, false, 1);
-        if(min==Integer.MAX_VALUE) {
-            System.out.println(-1);
-        } else {
-            System.out.println(min);
-        }
 
+        Queue<Cord> q = new LinkedList<>();
+        q.add(new Cord(0,0,false, 1));
+        map[0][0][0] = 1;
+        map[0][0][1] = 1;
+
+        while(q.size()!=0) {
+            Cord cur = q.poll();
+            if(cur.x==X-1 && cur.y==Y-1) {
+                System.out.println(cur.dist);
+                System.exit(0);
+            }
+            for (int i = 0; i < 4; i++) {
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+
+                if(0<=nx && nx<X && 0<=ny && ny<Y) {
+                    if(cur.skill) {
+                        if(map[nx][ny][1]==1) continue;
+                        map[nx][ny][1] = 1;
+                        q.add(new Cord(nx, ny, true, cur.dist+1));
+                    } else {
+                        if(map[nx][ny][0]==0) {
+                            map[nx][ny][0] = 1;
+                            q.add(new Cord(nx, ny, false, cur.dist+1));
+                        } else {
+                            q.add(new Cord(nx, ny, true, cur.dist+1));
+                        }
+                    }
+
+                }
+            }
+        }
+        System.out.println(-1);
 
     }
-    public static void dfs(int x, int y, boolean s, int dist) {
-        if(map[x][y]==1 && s) {
+    public static class Cord {
+        int x;
+        int y;
+        boolean skill;
+        int dist;
 
-        } else if(map[x][y]==1 && !s) {
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if(0<=nx && nx<X && 0<=ny && ny<Y) {
-                    s = true;
-                    dfs(nx, ny, s, dist+1);
-                    s = false;
-                }
-            }
-
-        } else if(x==X-1 && y==Y-1) {
-            min = Math.min(min,dist);
-        } else {
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if(0<=nx && nx<X && 0<=ny && ny<Y) {
-                    map[x][y] = 1;
-                    dfs(nx, ny, s, dist+1);
-                    map[x][y] = 0;
-                }
-            }
+        Cord(int x, int y, boolean skill, int dist) {
+            this.x = x;
+            this.y = y;
+            this.skill = skill;
+            this.dist = dist;
         }
     }
 
